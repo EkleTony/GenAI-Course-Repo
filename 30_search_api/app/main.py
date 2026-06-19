c4f9199ec391b79c8d4b1ea098c3a24e7ffaf0f2
ANTHONY EKLE <ekleanthony5@gmail.com>
2026-06-19 00:14:23 -0500

    updated app/main.py

---
+++ 30_search_api/app/main.py
@@ -0,0 +1,53 @@
+from fastapi import FastAPI
+import polars as pl
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics import DistanceMetric
+
+from app.functions import return_search_result_indexes
+
+
+# Load embedding model
+model_name = "all-MiniLM-L6-v2"
+model = SentenceTransformer(model_name)
+
+
+# Load video/document index
+df = pl.scan_parquet("app/data/video-index.parquet")
+
+
+# Distance metric
+dist = DistanceMetric.get_metric("manhattan")
+
+
+# create FastAPI app
+app = FastAPI()
+
+
+@app.get("/")
+def health_check():
+    return {"health_check": "OK"}
+
+
+@app.get("/info")
+def info():
+    return {
+        "name": "yt-search-api",
+        "description": "Semantic search API using FastAPI and sentence embeddings",
+    }
+
+
+@app.get("/search")
+def search(query: str):
+    result_indexes = return_search_result_indexes(query, df, model, dist)
+
+    results = (
+        df.select(["title", "video_id"])
+        .collect()
+        [result_indexes]
+        .to_dict(as_series=False)
+    )
+
+    return {
+        "query": query,
+        "results": results,
+    }
